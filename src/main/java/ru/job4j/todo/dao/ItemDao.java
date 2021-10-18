fcf9a79f07105879042cf69235163ea64e2a08ea
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-10-18 12:48:07 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/dao/ItemDao.java
+++ src/main/java/ru/job4j/todo/dao/ItemDao.java
@@ -2,14 +2,21 @@ package ru.job4j.todo.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.todo.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class ItemDao implements AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ItemDao.class.getName());
 
     private final StandardServiceRegistry registry;
 
@@ -25,48 +32,67 @@ public class ItemDao implements AutoCloseable {
     }
 
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+
+        );
     }
 
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item").list()
+        );
     }
 
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.get(Item.class, id)
+        );
     }
 
     public boolean replace(int id, Item item) {
-        boolean result = true;
-        try {
-            Session session = sf.openSession();
-            session.beginTransaction();
-            session.update(item);
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            result = false;
-        }
-        return result;
+        return this.tx(
+                session -> {
+                    boolean result = true;
+                    Query query = session.createQuery(
+                            "update Item set "
+                                    + "description = :description, "
+                                    + "created = :created, "
+                                    + "done = :done "
+                                    + "where id = :id");
+                    query.setParameter("description", item.getDescription());
+                    query.setParameter("created", item.getCreated());
+                    query.setParameter("done", item.isDone());
+                    query.setParameter("id", id);
+                    if (query.executeUpdate() > 0) {
+                        result = false;
+                    }
+                    return result;
+                }
+        );
     }
 
     @Override
     public void close() throws Exception {
         StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            LOG.error("Ошибка при работе с базой данных.", e);
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
